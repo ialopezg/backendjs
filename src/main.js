7d38abce5b65d514b503b8635b4d048c68f7686f
Isidro A. López G <me@ialopezg.com>
2022-05-05 08:46:35 -0500

    chore: Add Babel transpiler

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+import readline from 'readline';
+import { greeting } from './name';
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const name = reader.question('What is your name?: ', (name) => {
+    greeting(name);
+});
+
---
+++ src/name.js
@@ -0,0 +1,4 @@
+export const greeting = (name) => {
+  console.log('Hello %s!', name);
+  console.log('Nice to meet you!');
+};
